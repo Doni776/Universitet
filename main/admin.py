abb93f82bac473e090696342ca5f439d34099352
Doniyor <donyor6hamidov25@gmail.com>
2025-09-22 16:47:17 +0500

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Yonalish,Fan,Ustoz
+from django.contrib.auth.models import Group, User
 
-admin.site.register(Yonalish)
-admin.site.register(Fan)
-admin.site.register(Ustoz)
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ("id", "ism", "yosh", "jins", "daraja", "fan")
+    search_fields = ("ism",)
+
+class YonalishAdmin(admin.ModelAdmin):
+    list_display = ("id", "nom", "aktiv")
+    list_filter = ("aktiv",)
+    search_fields = ("nom",)
+
+class FanAdmin(admin.ModelAdmin):
+    list_display = ("id", "nom", "asosiy", "yonalish")
+    list_filter = ("asosiy", "yonalish")
+    search_fields = ("nom",)
+
+
+admin.site.register(Yonalish,YonalishAdmin)
+admin.site.register(Fan,FanAdmin)
+admin.site.register(Ustoz,UstozAdmin)
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+admin.site.site_header = "Universitet Admin paneli"
+admin.site.site_title = "Universitet boshqaruvi"
+admin.site.index_title = "Admin paneli"
